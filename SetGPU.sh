631bc58a02c0e6684cc8670872230d5def12df4c
yedushusheng <220143530@seu.edu.cn>
2018-11-06 22:47:08 +0800

    修改添加黑名单方式

--- SetGPU.sh
+++ SetGPU.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if test -e /etc/modprobe.d/blacklist.conf
-then
-	echo "blacklist amdgpu" >> /etc/modprobe.d/blacklist.conf
-else
-	touch /etc/modprobe.d/blacklist.conf
-	echo "blacklist amdgpu" >> /etc/modprobe.d/blacklist.conf
+sed -i '/blacklist amdgpu/d' /etc/modprobe.d/blacklist.conf>/dev/null>&1
+echo "blacklist amdgpu" >> /etc/modprobe.d/blacklist.conf
 	
 release_version = `uname -r`
 ramfs_version=initramfs-${release_version}.img
