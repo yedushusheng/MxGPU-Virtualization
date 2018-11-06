dcaa8260b9cf19f4d4371e5db5de4524055907f8
yedushusheng <220143530@seu.edu.cn>
2018-11-06 22:44:49 +0800

    设置ramfs_img

--- SetGPU.sh
+++ SetGPU.sh
@@ -8,6 +8,7 @@ else
 	echo "blacklist amdgpu" >> /etc/modprobe.d/blacklist.conf
 	
 release_version = `uname -r`
+ramfs_version=initramfs-${release_version}.img
 drucate -f -v --hostonly -k '/lib/modules/${release_version}/'
-				/boot/initramfs-${release_version}.img ${release_version}
+				/boot/ramfs_version ${release_version}
 reboot
