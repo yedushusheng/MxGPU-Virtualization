e8f8ef0d9b1a774ed8522ffe1a1737d67df8ff41
yedushusheng <yedushusheng@163.com>
2018-11-06 22:55:40 +0800

    设置脚本权限

--- SetGPU.sh
+++ SetGPU.sh
@@ -9,4 +9,6 @@ drucate -f -v --hostonly -k '/lib/modules/${release_version}/' /boot/ramfs_versi
 
 echo -e '#!/bin/sh\n/usr/sbin/modinfo -F filename gim' > /dev/null 2&>1 \nif [$? -eq 0]; then \n
 /sbin/modprobe gim \nfi' > /etc/sysconfig/modules/gim_modules
+chmod +x /etc/sysconfig/modules/gim_modules
+
 #reboot --unsafe
