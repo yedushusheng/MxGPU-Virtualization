25c20550e661c5596acf2f1ba8522eb1448a5ea5
yedushusheng <yedushusheng@163.com>
2018-11-06 22:54:36 +0800

    开机自动加载gim驱动

--- SetGPU.sh
+++ SetGPU.sh
@@ -6,4 +6,7 @@ echo "blacklist amdgpu" >> /etc/modprobe.d/blacklist.conf
 release_version = `uname -r`
 ramfs_version=initramfs-${release_version}.img
 drucate -f -v --hostonly -k '/lib/modules/${release_version}/' /boot/ramfs_version ${release_version}
+
+echo -e '#!/bin/sh\n/usr/sbin/modinfo -F filename gim' > /dev/null 2&>1 \nif [$? -eq 0]; then \n
+/sbin/modprobe gim \nfi' > /etc/sysconfig/modules/gim_modules
 #reboot --unsafe
